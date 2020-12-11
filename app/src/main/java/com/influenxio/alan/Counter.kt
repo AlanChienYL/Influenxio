66367b5fd35dfe6de7d0b5334eb3c922b33653dd
Alan Jian <jianyulun@qiyi.com>
2020-12-11 12:15:50 +0800

    add handleCustomer into counter to process job

--- app/src/main/java/com/influenxio/alan/Counter.kt
+++ app/src/main/java/com/influenxio/alan/Counter.kt
@@ -1,14 +1,44 @@
 package com.influenxio.alan
 
+import android.os.Handler
+import android.os.Message
+import androidx.annotation.WorkerThread
+import java.util.*
+import kotlin.concurrent.schedule
+
 data class Counter(
     var name: String? = null,
     var processing: String? = "idle",
-    var processed: String = ""
+    var processed: String = "",
+    private val handler: Handler
 ) {
+    var isRunning = false
+        private set
+    private var timerTask: TimerTask? = null
+
     override fun equals(other: Any?): Boolean {
         return when (other) {
             is Counter -> other.name == name
             else -> super.equals(other)
         }
+    }
+
+    fun handleCustomer(customer: Customer) {
+        isRunning = true
+        processing = customer.id.toString()
+        update()
+        timerTask = Timer().schedule(customer.processTime) {
+            processing = "idle"
+            processed += "${customer.id},"
+            isRunning = false
+            update()
+        }
+    }
+
+    @WorkerThread
+    private fun update() {
+        val message = Message()
+        message.obj = this
+        handler.sendMessage(message)
     }
 }
